721fffad961dfdaadea384e94d45e0dbded549db
Boina <nicolas.alarcon.14@sansano.usm.cl>
2017-10-16 17:39:05 -0300

    cliente comunicacion basica

---
+++ cliente.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class cliente {
+
+public static void main(String[] args) throws IOException {
+
+        //recordar de agregar la ip
+        if (args.length != 1) {
+                System.out.println("Usage: java cliente <hostname>");
+                return;
+        }
+
+        // get a datagram socket
+        DatagramSocket socket = new DatagramSocket();
+
+        // send request
+        byte[] buf = new byte[256];
+        InetAddress address = InetAddress.getByName(args[0]);
+
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
+        socket.send(packet);
+
+        // get response
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+
+        // display response
+        String received = new String(packet.getData(), 0, packet.getLength());
+        System.out.println("ip futura: " + received);
+
+        socket.close();
+}
+
+
+}
