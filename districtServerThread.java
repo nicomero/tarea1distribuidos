1bbf22a5cd8e29d41b6c3a01408ef2d3dcb3418e
Boina <nicolas.alarcon.14@sansano.usm.cl>
2017-10-18 10:29:16 -0300

    object oriented implemented in districtServer

--- districtServerThread.java
+++ districtServerThread.java
@@ -54,29 +54,39 @@ public districtServerThread(String name) throws IOException {
 
 public void run() {
         while (moreQuotes) {
-                try {
-                        byte[] buf = new byte[256];
 
                         // construct quote
                         String dString = null;
                         dString = new Date().toString();
 
-                        buf = dString.getBytes();
-
-                        // send it
-                        InetAddress group = InetAddress.getByName("230.0.0.1");
-                        DatagramPacket packet = new DatagramPacket(buf, buf.length, group, 4446);
-                        socket.send(packet);
+                        enviarU(dString, "230.0.0.1", socket);
 
                         // sleep for a while
                         try {
                                 sleep((long)(Math.random() * FIVE_SECONDS));
                         } catch (InterruptedException e) { }
-                } catch (IOException e) {
-                        e.printStackTrace();
-                        moreQuotes = false;
-                }
         }
         socket.close();
 }
+
+public void enviarU(String mensaje, String ip_destino, DatagramSocket socket){
+
+
+
+        try{
+                byte[] buf = new byte[256];
+
+                InetAddress address = InetAddress.getByName(ip_destino);
+                buf = mensaje.getBytes();
+                DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4446); //viene con puerto de defecto
+                socket.send(packet);
+        }catch(IOException e) {
+                System.out.println("ilprob");
+                e.printStackTrace();
+        }
+}
+
+
+
+
 }
